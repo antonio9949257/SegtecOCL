fd6ad08bf22df627b3b29810ca78af99089adb5b
Armin Dev Os <antonio9949257@gmail.com>
2024-11-05 08:50:11 +0100

    .

---
+++ auth/login.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const API_URL = 'https://segteosv.onrender.com/auth/login'; 
+
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const correo = document.getElementById('loginCorreo').value;
+    const contraseña = document.getElementById('loginContrasena').value;
+
+    try {
+        const response = await fetch(`${API_URL}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ correo, contraseña }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Inicio de sesión exitoso!');
+
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('NombreUsuario', data.nombre)
+
+            window.location.href = '../index.html'; 
+        } else {
+            alert(`Error: ${data.message}`);
+        }
+    } catch (error) {
+        console.error('Error en el inicio de sesión:', error);
+    }
+});
+});
---
+++ auth/register.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+const API_URL = 'https://segteosv.onrender.com/auth/register/cliente'; 
+
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const nombre = document.getElementById('registerNombre').value;
+    const apellido = document.getElementById('registerApellido').value;
+    const correo = document.getElementById('registerCorreo').value;
+    const telefono = document.getElementById('registerTelefono').value;
+    const contraseña = document.getElementById('registerContrasena').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (contraseña !== confirmPassword) {
+        alert('Las contraseñas no coinciden.');
+        return;
+    }
+
+    try {
+        const response = await fetch(`${API_URL}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nombre,
+                apellido,
+                correo,
+                telefono,
+                contraseña,
+            }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Registro exitoso!');
+            window.location.href = 'login.html';
+        } else {
+            alert(`Error: ${data.message}`);
+        }
+    } catch (error) {
+        console.error('Error en el registro:', error);
+    }
+});
+});
